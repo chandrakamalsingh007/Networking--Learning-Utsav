d9696172bc74074499b93259a88cb06b41256f0d
chandrakamalsingh007 <chandrakamalsingh007@gmail.com>
2025-10-26 19:19:02 +0545

    python

---
+++ Day-29/Python for Network Engineers/Built_in_Method.py
@@ -0,0 +1,1 @@
+#Built in Method in python
---
+++ Day-29/Python for Network Engineers/variables.py
@@ -0,0 +1,9 @@
+Router_type = "ASR9K"
+SW_version = "6.4.2"
+Number_of_Nodes = 15
+
+#print('Our network is made of ' + str(Number_of_Nodes) + " " + Router_type, " running S/W image " + SW_version)
+
+
+My_Network = f"Our network is made of {Number_of_Nodes}  {Router_type} running S/W image {SW_version}"
+print(My_Network)
